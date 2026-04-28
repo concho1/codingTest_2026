09ef53ada9654c6b50c6fb5aec31ea56a04cbd1b
concho1 <142205346+concho1@users.noreply.github.com>
2026-04-28 16:12:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 67.99 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,25 +2,18 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        HashMap<String, Integer> comMap = new HashMap<>();
-        for(String com : completion){
-            if(comMap.containsKey(com)){
-                comMap.put(com, comMap.get(com) + 1);
+        HashMap<String, Integer> compMap = new HashMap<>();
+        for(String comp : completion) 
+            compMap.put(comp, compMap.getOrDefault(comp, 0) + 1);
+        // 참가자들을 완주자 cnt 에서 --
+        for(String part : participant){
+            int cnt = compMap.getOrDefault(part, 0);
+            if(--cnt == -1){
+                return part;
             }else{
-                comMap.put(com, 1);
+                compMap.put(part, cnt);
             }
         }
-        for(String par : participant){
-            if(comMap.containsKey(par) && comMap.get(par) >= 1){
-                comMap.put(par, comMap.get(par)-1);
-            }else{
-                answer = par;
-                break;
-            }
-        }
-        
-        
-        return answer;
+        return "";
     }
 }
