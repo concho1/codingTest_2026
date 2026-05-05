9f0a4716d2a7c2f6ee32294454588f5196b62ec7
concho1 <142205346+concho1@users.noreply.github.com>
2026-05-05 15:13:34 +0900

    [level 2] Title: 전화번호 목록, Time: 380.50 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,20 +2,18 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        boolean answer = true;
         Arrays.sort(phone_book);
-        // 공백 제거
+        //System.out.println(Arrays.toString(phone_book));
         for(int i=0; i<phone_book.length; i++){
-            phone_book[i] = phone_book[i].replaceAll(" ", "");
-        }
-        // 정렬시 12 123 1235 567 88 이런식으로 prfix 가 앞으로 옴
-        for(int i=0; i<phone_book.length; i++){
-            String tg = phone_book[i];
             for(int j=i+1; j<phone_book.length; j++){
-                String tm = phone_book[j];
-                if(tm.startsWith(tg)) return false;
-                else break; // 이미 정렬 했음으로 다음게 prfix 가 아니면 넘어가기
+                if(phone_book[j].startsWith(phone_book[i])){
+                    return false;
+                }else{
+                    break;
+                }
             }
         }
-        return true;
+        return answer;
     }
 }
