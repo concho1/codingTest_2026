68b4d640b3f99bdc59056abd7f23e8b604cbc690
concho1 <142205346+concho1@users.noreply.github.com>
2026-05-05 16:21:27 +0900

    [level 1] Title: K번째수, Time: 1.09 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,31 +3,20 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        // 1. commands에 따라 i번째 숫자부터 j번째 숫자까지 자르고
-        // 2. 정렬 후 k번째에 있는 수를 구하기
-        int answerIdx = 0;
+        int aIdx = 0;
+        
         for(int[] com : commands){
-            int i = com[0];
-            int j = com[1];
-            int k = com[2];
-            int[] arr = cutAndSortArr(array, i, j);
-            answer[answerIdx] = arr[k-1];
-            answerIdx++;
+            int i = com[0]-1, j = com[1]-1, k = com[2]-1, len = com[1]-com[0]+1;
+            // 1 번 과정
+            int[] arr = new int[len];
+            for(int idx=i; idx<=j; idx++){
+                arr[idx-i] = array[idx];
+            }
+            // 2 번 과정
+            Arrays.sort(arr);
+            // 3 번 과정
+            answer[aIdx++] = arr[k];
         }
         return answer;
-    }
-    // 배열 자르기 및 정렬
-    static int[] cutAndSortArr(int[] array, int i, int j){
-        int length = j-i+1;
-        int[] result = new int[length];
-        int idx = 0;
-        // index out of range 방지용
-        if(j > array.length) j = array.length;
-        for(int n=i-1; n<j; n++){
-            result[idx] = array[n];
-            idx++;
-        }
-        Arrays.sort(result);
-        return result;
     }
 }
