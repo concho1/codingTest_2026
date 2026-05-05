246f201ba5576fc5fe55cb793726ff50c85e137e
concho1 <142205346+concho1@users.noreply.github.com>
2026-05-05 14:52:06 +0900

    [level 1] Title: 폰켓몬, Time: 4.89 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,10 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int n = nums.length/2;
-        HashSet<Integer> nSet = new HashSet<>();
-        for(int num : nums) nSet.add(num);
-        if(nSet.size() >= n) return n;
-        return nSet.size();
+        // 포켓몬 종류별로 몇마리인지 카운트
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        // 가져갈 수 있는 포켓몬 수
+        int ableCnt = nums.length/2;
+        int cataCnt = map.keySet().size();
+        // 포켓몬 종류 >= ableCnt 이면 ableCnt 만큼의 종류 가능
+        // 종류가 더 적으면 종류
+        return cataCnt >= ableCnt ? ableCnt : cataCnt;
     }
 }
