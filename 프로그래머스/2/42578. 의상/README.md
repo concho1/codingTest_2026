187a3fe26e3f33d8acde15c46a9a103a3b6ec1ce
concho1 <142205346+concho1@users.noreply.github.com>
2026-05-05 15:50:24 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 85.8 MB, 시간: 0.09 ms
+메모리: 82.5 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 16일 17:07:21
+2026년 05월 05일 15:50:21
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,24 +1,19 @@
 import java.util.*;
 
 class Solution {
-    static HashMap<String, Integer> map = new HashMap<>();
-    
     public int solution(String[][] clothes) {
         int answer = 1;
-        // 각 종류별로 최대 1가지
-        // 의상 종류별로 모으기
-        // 같은 이름을 가진 의상은 존재하지 않습니다. => 수 만 카운팅
-        // 종류 리스트
-        for(String[] strArr : clothes){
-            if(map.containsKey(strArr[1])){
-                map.put(strArr[1], map.get(strArr[1]) + 1);
-            }else{
-                map.put(strArr[1], 2);  // 안입는 경우 추가
-            }
+        // 종류별 옷 개수 카운팅
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String[] cloth : clothes){
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
         }
-        
-        for(String k : map.keySet()){
-            answer *= map.get(k);
+        // 종류 별로 2, 1 개면
+        // (2+1) x (1+1) - 1 = 6-1 = 5
+        // 종류별로 3 개면
+        // (3+1) - 1 = 3
+        for(Integer cnt : map.values()){
+            answer *= (cnt + 1);
         }
         
         return answer-1;
